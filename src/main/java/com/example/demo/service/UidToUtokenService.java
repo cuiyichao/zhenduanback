f48f1c0d051b0840c743fbeb9ba67fbcd433c3d6
cuiyichao <59215829+cuiyichao@users.noreply.github.com>
2025-02-26 15:49:25 +0800

    Update UidToUtokenService.java

--- src/main/java/com/example/demo/service/UidToUtokenService.java
+++ src/main/java/com/example/demo/service/UidToUtokenService.java
@@ -1,18 +1,33 @@
 package com.example.demo.service;
 
 import com.example.demo.util.PhpApiEncodeUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UidToUtokenService {
+
+    private static final Logger logger = LoggerFactory.getLogger(UidToUtokenService.class);
+
     /**
-     * 根据uid生成utoken
-     * @param uid 学员uid
-     * @param tokenKey 加密密钥
+     * 根据 uid 和 tokenKey 生成 utoken
+     * @param uid 学员 uid
+     * @param tokenKey 令牌密钥
      * @return utoken
-     * @throws Exception 异常
      */
-    public String convertUidToUtoken(String uid, String tokenKey) throws Exception {
-        return PhpApiEncodeUtil.encoded(uid, tokenKey);
+    public String convertUidToUtoken(String uid, String tokenKey) {
+        try {
+            System.out.println("Received uid: env: "+uid+":"+ tokenKey);
+            String utoken = PhpApiEncodeUtil.encoded(uid, tokenKey);
+            logger.info("生成的 utoken: {}", utoken);
+            System.out.println("生成的 utoken: {}"+ utoken);
+            return utoken;
+        } catch (Exception e) {
+            System.out.println("生成 utoken 时发生异常，uid: {}, tokenKey: {}"+uid+":"+ tokenKey+":"+ e);
+            System.out.println(e.toString());
+            logger.error("生成 utoken 时发生异常，uid: {}, tokenKey: {}", uid, tokenKey, e);
+            return null;
+        }
     }
 }
